5ed73f2909c07fcb8f407528949dd112be4431c5
andluo <1455234749@qq.com>
2019-02-24 23:43:36 +0800

    完成整体功能

--- js/index2.js
+++ js/index2.js
@@ -108,37 +108,174 @@ class Top250{
 
 class Us{
     constructor(){
-
+        this.$container = $('#us')
     }
     
     init(){
         console.log("init us")
-
+        this.bind()
+        this.start()
     }
     
     bind(){
 
     }
     start(){
-        
+        this.getData((data)=>{
+            this.render(data)
+        })
+       
+    }
+
+    getData(fn){
+        //获取页面数据
+
+        $.ajax({
+            url:'//api.douban.com/v2/movie/us_box',
+            type:'GET',
+            dataType:'jsonp',
+            
+            
+        }).done((res)=>{
+            console.log('请求US电影数据成功:')
+            fn(res)
+            console.log(res)
+        }).fail((res)=>{
+            console.log('请求电影数据失败: ')
+            console.log(res)    
+        })
+    }
+
+    render(data){
+        data.subjects.forEach((movie)=>{
+            movie = movie.subject
+            let {casts,directors} = movie
+            let cast_name_arr = []
+            let director_name_arr = []
+            casts.forEach((cast)=>{
+                cast_name_arr.push(cast.name)
+            })
+            directors.forEach((director)=>{
+                director_name_arr.push(director.name)
+            })
+    
+            let tmp = `            <div class="item">
+            <a href="#">
+                <div class="cover">
+                    <img src="${movie.images.medium}" alt="">
+                </div>
+                <div class="detail">
+                    <h2>${movie.title}</h2>
+                    <div class="extra">
+                        <span class="score">${movie.rating.average}</span> / ${movie.collect_count}收藏
+                    </div>
+                    <div class="extra">${movie.year} / ${movie.genres.join('、')}</div>
+                    <div class="extra">导演：${director_name_arr.join('、')}</div>
+                    <div class="extra">主演：${cast_name_arr.join('、')}</div>
+    
+                </div>
+            </a>
+        </div>
+    `
+             let $node = $(tmp)
+             this.$container.append($node)
+        })
     }
 }
 
 class Search{
     constructor(){
-
+        this.$element = $('#search')
+        this.$container = $('.search-reslut .container')
     }
     
     init(){
         console.log("init search")
+        this.bind()
+        this.start()
+        this.keyword = null;
 
     }
     
     bind(){
-
+        this.$element.find('.button').click(()=>{
+            this.keyword = this.$element.find('input').val()
+            this.$container.empty()
+            this.getData((data)=>{
+                this.render(data)
+            })
+        })
+            
     }
     start(){
         
+    }
+
+    getData(fn){
+        //获取页面数据
+        if(this.is_lodding){
+            console.log('lodding中，不重新请求数据')
+            return
+        }else{
+            this.is_lodding = true
+            this.$element.find('.loading').show()
+
+            $.ajax({
+                url:'//api.douban.com/v2/movie/search',
+                type:'GET',
+                dataType:'jsonp',
+                data: {
+                    q:this.keyword
+                }
+                
+            }).done((res)=>{
+                console.log('搜索电影数据成功:')
+                fn(res)
+                console.log(res)
+            }).fail((res)=>{
+                console.log('搜索电影数据失败: ')
+                console.log(res)    
+            }).always(()=>{
+                this.is_lodding = false
+                this.$element.find('.loading').hide()
+    
+            })
+        }
+    }
+
+    render(data){
+        data.subjects.forEach((movie)=>{
+            let {casts,directors} = movie
+            let cast_name_arr = []
+            let director_name_arr = []
+            casts.forEach((cast)=>{
+                cast_name_arr.push(cast.name)
+            })
+            directors.forEach((director)=>{
+                director_name_arr.push(director.name)
+            })
+    
+            let tmp = `            <div class="item">
+            <a href="#">
+                <div class="cover">
+                    <img src="${movie.images.medium}" alt="">
+                </div>
+                <div class="detail">
+                    <h2>${movie.title}</h2>
+                    <div class="extra">
+                        <span class="score">${movie.rating.average}</span> / ${movie.collect_count}收藏
+                    </div>
+                    <div class="extra">${movie.year} / ${movie.genres.join('、')}</div>
+                    <div class="extra">导演：${director_name_arr.join('、')}</div>
+                    <div class="extra">主演：${cast_name_arr.join('、')}</div>
+    
+                </div>
+            </a>
+        </div>
+    `
+             let $node = $(tmp)
+             this.$container.append($node)
+        })
     }
 }
 
