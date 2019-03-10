571183889e04a042cf195f9a7f70fccf8a6c5845
andluo <1455234749@qq.com>
2019-03-11 00:21:48 +0800

    US模块化优化

--- src/index.js
+++ src/index.js
@@ -25,7 +25,6 @@ class App{
         this.$tabs = $('footer>div') //tab按钮
         this.panels = $('section')
         this.top250 = top250
-        this.us = us
         this.search = search
         this.fav = fav
         this.detail = detail
@@ -35,7 +34,6 @@ class App{
         // 初始化
         this.bind()
         this.top250.init()
-        this.us.init()
         this.search.init()
         this.fav.init()
         this.detail.init()
@@ -52,6 +50,10 @@ class App{
             if ($(this).data('id') === 'xihuan'){
                 _this.fav.render()
             }
+
+            if( index === 1 && $('.waterfall img').length === 0){
+                new Us().init() 
+            }
             
         })
 
@@ -63,7 +65,6 @@ class App{
 let app = new App(
     {
         top250:new Top250(),
-        us:new Us(),
         search:new Search(),
         fav:new Favorite(),
         detail:new Detail()
@@ -73,94 +74,8 @@ let app = new App(
 
 
 
-$('footer>div').eq(1).click(()=>{
-    
-    if($('.waterfall img').length === 0){
-        getData(render)
-    }
-    
-    
-})
 
 
-function getData(fn){
-    //获取页面数据
-    $('.waterfall').parent().find('.loading').show()
-
-    $.ajax({
-        // url:'//api.douban.com/v2/movie/us_box',
-        url:'//api.douban.com/v2/movie/in_theaters',
-        type:'GET',
-        dataType:'jsonp',
-        
-        
-    }).done((res)=>{
-        fn(res)
-        console.log(res)
-    }).fail((res)=>{
-        console.log('请求电影数据失败: ')
-        console.log(res)    
-    }).always(()=>{
-        $('.waterfall').parent().find('.loading').hide()
-
-    })
-}
-
-function render(data){
-    let height_arr = [70,80,90,120]
-    data.subjects.forEach((movie)=>{
-        //  movie = movie.subject
-
-         let tmp = `
-         <div class="img-wrap" data-movie-id=${movie.id}>
-            <img src="http://img3.doubanio.com/f/movie/b6dc761f5e4cf04032faa969826986efbecd54bb/pics/movie/movie_default_small.png"  data-src = "${movie.images.small}" alt="">
-            <span class="score">${Number(movie.rating.average).toFixed(1)}<span>
-         </div>
-         `
-         
-         let $elemnet = $(tmp)
-         $elemnet.find('img').css('height',height_arr[Math.floor(Math.random()*4)])
-         $('.waterfall').append($elemnet)
-    })
-    layout()
-}
-
-function layout(){
-    //瀑布流布局
-    let colHeightArray = []
-    // let imgWidth = $('.waterfall img').outerWidth(true)
-    let imgWidth = $('.waterfall .img-wrap').outerWidth(true)
-
-    let colCount =  Math.floor($('.waterfall').width()/imgWidth)
-    for(let i=0; i<colCount; i++){
-        colHeightArray[i] = 0
-    }
-    console.log(`${colHeightArray} ${imgWidth} ${colCount}`)
-    $('.waterfall .img-wrap').each(function(){
-        let min_value = Math.min(...colHeightArray)
-        let min_index = colHeightArray.indexOf(min_value)
-        $(this).css({
-            left:min_index*imgWidth,
-            top:min_value
-        })
-        colHeightArray[min_index] += $(this).outerHeight(true)
-    })
-
-    //懒加载图片
-    $('.waterfall img').not('[data-isLoaded]').each(function(){
-        if( $('main').height()+$('main').scrollTop() > $(this).offset().top ){
-            $(this).attr('src',$(this).attr('data-src'))
-            $(this).attr('data-isLoaded',1)
-        }
-    })
-
-    //居中对齐
-    let total_width = $('.waterfall .img-wrap').parents('section').outerWidth(true)
-    let imgs_width = $('.waterfall .img-wrap').eq(0).outerWidth(true) * colCount
-    let padding = Math.floor( (total_width - imgs_width)/2 )
-    $('.waterfall .img-wrap').parents('section').css({'padding-left':padding,'padding-top':padding})
-    console.log(`padding-left is ${padding}`)
-}
 
 
 
--- src/js/us.js
+++ src/js/us.js
@@ -15,6 +15,7 @@ class Us{
     
     init(){
         this.bind()
+        this.getData()
         
     }
     
@@ -25,7 +26,83 @@ class Us{
         })
     }
 
-
+    getData(){
+        //获取页面数据
+        $('.waterfall').parent().find('.loading').show()
+    
+        $.ajax({
+            // url:'//api.douban.com/v2/movie/us_box',
+            url:'//api.douban.com/v2/movie/in_theaters',
+            type:'GET',
+            dataType:'jsonp',
+            
+            
+        }).done((res)=>{
+            this.render(res)
+        }).fail((res)=>{
+            console.log('请求电影数据失败: ')
+            console.log(res)    
+        }).always(()=>{
+            $('.waterfall').parent().find('.loading').hide()
+    
+        })
+    }
+    
+     render(data){
+        let height_arr = [70,80,90,120]
+        data.subjects.forEach((movie)=>{
+            //  movie = movie.subject
+    
+             let tmp = `
+             <div class="img-wrap" data-movie-id=${movie.id}>
+                <img src="http://img3.doubanio.com/f/movie/b6dc761f5e4cf04032faa969826986efbecd54bb/pics/movie/movie_default_small.png"  data-src = "${movie.images.small}" alt="">
+                <span class="score">${Number(movie.rating.average).toFixed(1)}<span>
+             </div>
+             `
+             
+             let $elemnet = $(tmp)
+             $elemnet.find('img').css('height',height_arr[Math.floor(Math.random()*4)])
+             $('.waterfall').append($elemnet)
+        })
+        this.layout()
+    }
+    
+    layout(){
+        //瀑布流布局
+        let colHeightArray = []
+        // let imgWidth = $('.waterfall img').outerWidth(true)
+        let imgWidth = $('.waterfall .img-wrap').outerWidth(true)
+    
+        let colCount =  Math.floor($('.waterfall').width()/imgWidth)
+        for(let i=0; i<colCount; i++){
+            colHeightArray[i] = 0
+        }
+        console.log(`${colHeightArray} ${imgWidth} ${colCount}`)
+        $('.waterfall .img-wrap').each(function(){
+            let min_value = Math.min(...colHeightArray)
+            let min_index = colHeightArray.indexOf(min_value)
+            $(this).css({
+                left:min_index*imgWidth,
+                top:min_value
+            })
+            colHeightArray[min_index] += $(this).outerHeight(true)
+        })
+    
+        //懒加载图片
+        $('.waterfall img').not('[data-isLoaded]').each(function(){
+            if( $('main').height()+$('main').scrollTop() > $(this).offset().top ){
+                $(this).attr('src',$(this).attr('data-src'))
+                $(this).attr('data-isLoaded',1)
+            }
+        })
+    
+        //居中对齐
+        let total_width = $('.waterfall .img-wrap').parents('section').outerWidth(true)
+        let imgs_width = $('.waterfall .img-wrap').eq(0).outerWidth(true) * colCount
+        let padding = Math.floor( (total_width - imgs_width)/2 )
+        $('.waterfall .img-wrap').parents('section').css({'padding-left':padding,'padding-top':padding})
+        console.log(`padding-left is ${padding}`)
+    }
 
 
 
